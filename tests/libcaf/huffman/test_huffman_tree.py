d3fb4f33bafe830e06a013f231c8782f9202dd32
Daniel-Shamir <daniel.shamir.work@gmail.com>
2025-12-19 15:29:16 +0200

    fixed test to account for the new structure

--- tests/libcaf/huffman/test_huffman_tree.py
+++ tests/libcaf/huffman/test_huffman_tree.py
@@ -1,7 +1,7 @@
 import pytest
 from libcaf import histogram, huffman_tree, HuffmanNode
 
-def assert_is_node_valid(node: HuffmanNode) -> bool:
+def assert_node_is_valid(node: HuffmanNode) -> bool:
     assert node is not None
     assert isinstance(node.frequency, int)
     assert node.frequency >= 0
@@ -12,27 +12,20 @@ def test_huffman_tree() -> None:
     hist = histogram(input_bytes)
     nodes, root_index = huffman_tree(hist)
 
-    for index, node in enumerate(nodes):
-        assert_is_node_valid(node)
+    root_is_not_child = True
 
-        # symbol = node.symbol
-        # frequency = node.frequency
-        # left_index = node.left_child_index
-        # right_index = node.right_child_index
-        # is_leaf = (left_index == HUFFMAN_NODE_NULL_INDEX) and (right_index == HUFFMAN_NODE_NULL_INDEX)
+    for node in nodes:
+        assert_node_is_valid(node)
 
-        # assert frequency > 0
+        if node.is_leaf: # leaf node
+            assert node.symbol in input_bytes
+            assert node.frequency == input_bytes.count(bytes([node.symbol]))
+        else: # internal node
+            assert node.frequency == nodes[node.left_index].frequency + nodes[node.right_index].frequency
+            if node.left_index == root_index or node.right_index == root_index:
+                root_is_not_child = False
 
-        # if is_leaf: # leaf node
-        #     assert left_index == HUFFMAN_NODE_NULL_INDEX
-        #     assert right_index == HUFFMAN_NODE_NULL_INDEX
-        #     assert symbol in input_bytes
-        #     assert frequency == input_bytes.count(bytes([symbol]))
-        # else: # internal node
-        #     # NOTE(daniel): i believe huffman trees are always complete, but i don't want to assume
-        #     # NOTE(daniel): an internal node's symbol is unused, no need to check it
-        #     assert left_index != HUFFMAN_NODE_NULL_INDEX or right_index != HUFFMAN_NODE_NULL_INDEX
-        #     assert frequency == nodes[left_index].frequency + nodes[right_index].frequency
+    assert root_is_not_child
 
 if __name__ == "__main__":
     test_huffman_tree()
